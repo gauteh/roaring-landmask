0880daabd823af55d03de585e5932161919890fe
Gaute Hope <gaute.hope@met.no>
2021-09-06 12:04:58 +0200

    tests: skip o-l-d tests if uninstalled

--- tests/test_old.py
+++ tests/test_old.py
@@ -1,7 +1,19 @@
+import pytest
 import numpy as np
 from roaring_landmask import RoaringLandmask
-from opendrift_landmask_data import Landmask
 
+try:
+  from opendrift_landmask_data import Landmask
+  has_old = True
+except ImportError:
+  has_old = False
+
+has_old = pytest.mark.skipif(
+    has_old is False,
+    reason = "opendrift-landmask-data is not installed"
+    )
+
+@has_old
 def test_landmask_vs_o_l_d():
   l = RoaringLandmask.new()
   ol = Landmask()
